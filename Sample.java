d5de12a99a509cfa7957fc314a63bd83cec756c9
Greg Nicotera <42983801+gregnicotera@users.noreply.github.com>
2018-11-09 04:14:12 -0500

    Update Sample.java

--- Sample.java
+++ Sample.java
@@ -1,11 +1,3 @@
-/******************************************************************************\
-* Copyright (C) 2012-2016 Leap Motion, Inc. All rights reserved.               *
-* Leap Motion proprietary and confidential. Not for distribution.              *
-* Use subject to the terms of the Leap Motion SDK Agreement available at       *
-* https://developer.leapmotion.com/sdk_agreement, or another agreement         *
-* between Leap Motion and you, your company or other organization.             *
-\******************************************************************************/
-
 import java.io.File;
 import java.io.IOException;
 import java.lang.Math;
